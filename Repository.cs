4b46ebc478436091b4f452dc13c86449334040bd
Davey van Tilburg <davey@pestscan.eu>
2022-04-20 11:15:37 +0200

    Add repository.cs

---
+++ Repository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PCSService
+{
+    public class Repository <T> where T : IEntity, new()
+    {
+        private readonly string _getStoredProcedureName;
+
+        public Repository(string getStoredProcedureName)
+        {
+            _getStoredProcedureName = getStoredProcedureName;
+        }
+
+        public IEnumerable<T> Select()
+        {
+            using (SqlConnection cn = CData.GetConnection())
+            {
+                using (SqlCommand com = new SqlCommand(_getStoredProcedureName, cn))
+                {
+                    com.CommandType = CommandType.StoredProcedure;
+
+                    cn.Open();
+                    SqlDataReader reader = com.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        var item = new T();
+                        item.Set(reader);
+
+                        yield return item;
+                    }
+                }
+            }
+        }
+    }
+
+    public interface IEntity
+    {
+        void Set(SqlDataReader reader);
+    }
+}

12896d8dc9b62b53d059131b27f42fe1b7115b2b
Davey van Tilburg <davey@pestscan.eu>
2022-04-20 11:32:38 +0200

    add sqldatareader

---
+++ SqlDataReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace PestScan.Common.ExtensionMethods
+{
+    public static class SqlDataReaderExtensions
+    {
+        public static T Read<T>(this SqlDataReader reader, string columnName)
+        {
+            object columnValue = reader[columnName];
+
+            if (columnValue == null || columnValue == DBNull.Value)
+                return default;
+
+            return reader.GetFieldValue<T>(reader.GetOrdinal(columnName));
+        }
+    }
+}
